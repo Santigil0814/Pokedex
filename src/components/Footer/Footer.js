7c8d76c70c330a364b752404176a4a98f0030456
Santiago Gil Ramirez <santigil0814@gmail.com>
2021-10-30 09:21:20 -0500

    hola

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => (
+    <footer> 
+        <div>
+            Creado por <a href="https://www.instagram.com/santigilr?r=nametag">Santiago Gil</a>
+        </div>
+        <div>
+            <a href="https://github.com/Santigil0814">Visita mi Github</a>
+        </div>
+    </footer>
+    )
+
+
+export default Footer
